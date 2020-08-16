d539c4e56fd65133ba8fe45bb6321d8905528c9f
Hurt <shinb12138@163.com>
2020-08-16 21:14:18 +0800

    PYtest第2次作业 计算器测试补充,以及测试报告

--- test_one/conftest.py
+++ test_one/conftest.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from typing import List
 
+import pytest
+
 
 def pytest_collection_modifyitems(
         session: "Session", config: "Config", items: List["Item"]
@@ -8,3 +10,12 @@ def pytest_collection_modifyitems(
     for item in items:
         item.name = item.name.encode('utf-8').decode('unicode-escape')
         item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
+
+
+
+
+@pytest.fixture(scope='module', autouse=True)
+def firstone():
+    print("计算开始")
+    yield
+    print("计算结束")
--- test_one/test_calculator.py
+++ test_one/test_calculator.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
-from test_one.Calculator import Cal
+import allure
 import pytest
 import yaml
+
+from test_one.Calculator import Cal
+
+
+@pytest.fixture()
+def funt():
+    print("开始")
+    yield
+    print("结束")
 
 
 def get_adddata():
@@ -11,7 +20,7 @@ def get_adddata():
         ids = mydatas['add']['ids']
     return [adddatas, ids]
 
-
+@allure.feature("加法测试")
 class Test_add:
 
     def setup_class(self):
@@ -20,14 +29,35 @@ class Test_add:
 
     def teardown_class(self):
         print("加法测试结束")
-    def setup(self):
-        print("开始计算")
-    def teardown(self):
-        print("结束计算")
+
+    @pytest.mark.run(order=1)
     @pytest.mark.parametrize('a,b,result', get_adddata()[0], ids=get_adddata()[1])
-    def test_add(self, a, b, result):
+    @allure.story(f"{get_adddata()[1]}")
+    def test_add(self, a, b, result,funt):
         assert result == round(self.cal.add(a, b), 2)
 
+def get_divdata():
+    with open('./cal.yml', encoding='utf-8') as f:
+        mydatas = yaml.safe_load(f)
+        divdatas = mydatas['div']['datas']
+        ids = mydatas['div']['ids']
+    return [divdatas, ids]
+
+@allure.feature("除法测试")
+class Test_div:
+
+    def setup_class(self):
+        print("除法测试开始")
+        self.cal = Cal()
+
+    def teardown_class(self):
+        print("除法测试结束")
+
+    @pytest.mark.run(order=4)
+    @pytest.mark.parametrize('a,b,result', get_divdata()[0], ids=get_divdata()[1])
+    @allure.story(f"{get_adddata()[1]}")
+    def test_div(self, a, b, result,funt):
+        assert result == round(self.cal.div(a, b), 2)
 
 def get_subdata():
     with open('./cal.yml', encoding='utf-8') as f:
@@ -36,7 +66,7 @@ def get_subdata():
         ids = mydatas['sub']['ids']
     return [subdatas, ids]
 
-
+@allure.feature("减法测试")
 class Test_sub:
 
     def setup_class(self):
@@ -45,13 +75,11 @@ class Test_sub:
 
     def teardown_class(self):
         print("减法测试结束")
-    def setup(self):
-        print("开始计算")
-    def teardown(self):
-        print("结束计算")
 
+    @pytest.mark.run(order=2)
     @pytest.mark.parametrize('a,b,result', get_subdata()[0], ids=get_subdata()[1])
-    def test_sub(self, a, b, result):
+    @allure.story(f"{get_adddata()[1]}")
+    def test_sub(self, a, b, result,funt):
         assert result == round(self.cal.sub(a, b), 2)
 
 
@@ -62,7 +90,7 @@ def get_muldata():
         ids = mydatas['mul']['ids']
     return [muldatas, ids]
 
-
+@allure.feature("乘法测试")
 class Test_mul:
 
     def setup_class(self):
@@ -72,39 +100,12 @@ class Test_mul:
     def teardown_class(self):
         print("乘法测试结束")
 
-    def setup(self):
-        print("开始计算")
-    def teardown(self):
-        print("结束计算")
 
+    @pytest.mark.run(order=3)
     @pytest.mark.parametrize('a,b,result', get_muldata()[0], ids=get_muldata()[1])
-    def test_mul(self, a, b, result):
+    @allure.story(f"{get_adddata()[1]}")
+    def test_mul(self, a, b, result,funt):
         assert result == round(self.cal.mul(a, b), 2)
 
 
-def get_divdata():
-    with open('./cal.yml', encoding='utf-8') as f:
-        mydatas = yaml.safe_load(f)
-        divdatas = mydatas['div']['datas']
-        ids = mydatas['div']['ids']
-    return [divdatas, ids]
 
-
-class Test_div:
-
-    def setup_class(self):
-        print("除法测试开始")
-        self.cal = Cal()
-
-    def teardown_class(self):
-        print("除法测试结束")
-
-
-    def setup(self):
-        print("开始计算")
-    def teardown(self):
-        print("结束计算")
-
-    @pytest.mark.parametrize('a,b,result', get_divdata()[0], ids=get_divdata()[1])
-    def test_div(self, a, b, result):
-        assert result == round(self.cal.div(a, b), 2)
